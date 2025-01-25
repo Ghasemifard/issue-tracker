f65bc0b4d75c504f5cf248b7f87a96a920c311d5
Ghasemifard <ghasemifard_mehdi@yahoo.com>
2025-01-26 00:20:42 +0330

    Add aditional loading skeletons

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,29 +1,27 @@
-import { notFound } from "next/navigation";
-import prisma from "@/prisma/client";
-import delay from "delay";
-import { Card, Flex, Heading,Text } from "@radix-ui/themes";
 import IssueStatusBadge from "@/app/components/IssueStatusBadge";
-import ReactMarkdown from 'react-markdown'
+import prisma from "@/prisma/client";
+import { Card, Flex, Heading, Text } from "@radix-ui/themes";
+import { notFound } from "next/navigation";
+import ReactMarkdown from 'react-markdown';
+
 interface Props {
   params: { id: string };
 }
 
 const IssueDetailPage = async ({ params }: Props) => {
-  // Asynchronously unwrap params
+
   const { id } = await params;
-  // Parse and validate id
+
   const issueId = parseInt(id, 10);
   if (isNaN(issueId)) {
-    notFound(); // Redirect to 404 if id is invalid
+    notFound();
   }
     
-  // Fetch the issue from the database
   const issue = await prisma.issue.findUnique({
     where: { id: issueId },
   });
-  if (!issue) notFound(); //Redirect to 404 if no issue is found
-  // Optional delay for demonstration purposes
-  await delay(2000);
+  if (!issue) notFound();
+
 
   return (
     <div>
